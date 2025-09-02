Mixed precision caching bug, more testing for broken loss bug

acai_omr/models/kv_caching.py
@@ -18,7 +18,8 @@ class KVCache(nn.Module):
         max_seq_len (int): maximum sequence length model will be run with
         num_kv_heads (int): number of key/value heads.
         head_dim (int): per-attention head embedding dimension
-        dtype (torch.dtype): dtype for the caches
+        dtype (torch.dtype): dtype for the caches. This manually has to be set to a lower precision type
+        if using autocast
     """
 
     def __init__(
@@ -137,7 +138,6 @@ class CachedMultiheadAttention(nn.MultiheadAttention):
         attention_output = self.out_proj(attention_output) 
         return attention_output
 
-# and then subclass a TransformerDecoder class using this, and then subclass OMRDecoder to have a cached variant using that TransformerDecoder
 class CachedTransformerDecoderLayer(nn.TransformerDecoderLayer):
     def __init__(
         self,
@@ -240,7 +240,6 @@ class MemoryCache(nn.Module):
         batch_size = memory.shape[0]
         memory_len = memory.shape[1]
 
-        print(f"DEBUG: W_kv being used in cross cache: {self.W_kv[0]}")
         KV_cross = F.linear(memory, self.W_kv, self.b_kv)
         K_cross, V_cross = KV_cross.chunk(2, dim=-1)
         # split the last (embedding) dimension into heads, then transpose latent_len and num_heads to match sdpa's expected dim order.
@@ -269,13 +268,14 @@ class CachedTransformerDecoder(nn.TransformerDecoder):
             decoder_layer: CachedTransformerDecoderLayer, 
             num_layers: int, 
             max_batch_size: int, 
-            max_decoder_seq_len: int, 
+            max_decoder_seq_len: int,
+            cache_dtype, 
             norm: nn.Module = None):
         assert isinstance(decoder_layer, CachedTransformerDecoderLayer), "Can't use uncached TransformerDecoderLayer in a cached TransformerDecoder"
         super().__init__(decoder_layer, num_layers, norm)
        
         # initialize caches for each layer 
-        self.self_attn_caches = nn.ModuleList([KVCache(max_batch_size, max_decoder_seq_len, layer.num_heads, layer.head_dim, dtype=torch.bfloat16) for layer in self.layers])
+        self.self_attn_caches = nn.ModuleList([KVCache(max_batch_size, max_decoder_seq_len, layer.num_heads, layer.head_dim, dtype=cache_dtype) for layer in self.layers])
         self.cross_attn_caches = nn.ModuleList([MemoryCache(layer) for layer in self.layers])
 
     # reset each layer's self and cross attention caches. Fill cross attention caches using this batch's memory

acai_omr/models/models.py
@@ -375,9 +375,9 @@ class FineTuneOMREncoder(OMREncoder):
         return x
 
 class OMRDecoder(nn.Module):
-    # if use_caching = True, we use cached decoder layers and caches are set up with max_batch_size (which has to be 
-    # changed from its default of None). Otherwise, we use vanilla pytorch layers (and max_batch_size does nothing)
-    def __init__(self, max_lmx_seq_len, lmx_vocab_path, num_layers=10, hidden_dim=1024, num_heads=16, mlp_dim=4096, transformer_dropout=0.1, use_caching=False, max_batch_size=None):
+    # if use_caching = True, we use cached decoder layers and caches are set up with max_batch_size and a dtype of cache_dtype
+    # (which have to be changed from their defaults of None). Otherwise, we use vanilla pytorch layers (and cache args do nothing)
+    def __init__(self, max_lmx_seq_len, lmx_vocab_path, num_layers=10, hidden_dim=1024, num_heads=16, mlp_dim=4096, transformer_dropout=0.1, use_caching=False, max_batch_size=None, cache_dtype=None):
         super().__init__()
         self.max_lmx_seq_len = max_lmx_seq_len
         self.lmx_vocab_path = lmx_vocab_path
@@ -414,6 +414,7 @@ class OMRDecoder(nn.Module):
                 num_layers=num_layers,
                 max_batch_size=max_batch_size,
                 max_decoder_seq_len=max_lmx_seq_len,
+                cache_dtype=cache_dtype,
                 norm=nn.LayerNorm(self.hidden_dim, eps=1e-6)
             )
         else:
@@ -426,7 +427,7 @@ class OMRDecoder(nn.Module):
         self.unembed = nn.Linear(self.hidden_dim, self.vocab_size)
 
     # utility function to return a cachable OMRDecoder that's otherwise identical to this instance
-    def to_cached_version(self, max_batch_size):
+    def to_cached_version(self, max_batch_size, cache_dtype):
         return OMRDecoder(
             self.max_lmx_seq_len,
             self.lmx_vocab_path,
@@ -436,7 +437,8 @@ class OMRDecoder(nn.Module):
             self.mlp_dim,
             self.transformer_dropout,
             use_caching=True,
-            max_batch_size=max_batch_size
+            max_batch_size=max_batch_size,
+            cache_dtype=cache_dtype,
         )
 
     def forward(self, input_seqs, img_latent, lmx_attention_mask, latent_attention_mask, token_idxs_input=True, checkpoint_grads=False):
@@ -884,13 +886,12 @@ class GRPOViTOMR(nn.Module):
         rollout_mask: (R, T) tensor where True = token is part of a rollout, False = token isn't
     For each (T, E_enc) image latent in img_latent, create group_size autoregressive rollouts according to the
     policy defined by the model's outputted distributions, up to max_actions steps in total (where <bos> stems count as 1 action) for each. 
-    At each autoregressive step, set logits that aren't in the top_k top logits to -inf, then apply softmax with temperature, 
+    At each autoregressive step, select the topk logits, then apply softmax with temperature, 
     then extend the sequence according to the resulting distribution 
     Note: predictions after first train seem pretty peaky so default is to use temperature > 1 to slightly smooth things and 
     encourage more exploration.
     Also note that this should be called with torch_no_grad by the old policy and img_latent and latent_attention_mask should
     be the result of self.expand_img_tensors_for_rollout
- 
     """
     def cached_forward_rollout_policy(self, img_latent, latent_attention_mask, max_actions=768, top_k=50, temperature=1.2):
         device = img_latent.device

acai_omr/train/omr_grpo_train.py
@@ -14,8 +14,11 @@ from torchvision.transforms import v2, InterpolationMode
 from olimpic_app.evaluation.TEDn_lmx_xml import TEDn_lmx_xml
 from pathlib import Path
 import pandas as pd
-import copy
 import time
+
+# TODOS: fix theta_logits from teacher force step not having requires_grad = True (likely a problem introduced with caching)
+# back on gpu machine, test that old policy doesnt update between update steps but is being synced between batches
+# using debug print
 
 MODEL_DIR_PATH = Path("grpo_omr_train")
 CHECKPOINTS_DIR_PATH = MODEL_DIR_PATH / "checkpoints"
@@ -25,8 +28,8 @@ AUGMENTATION_P = 0.25
 
 TRAIN_BATCH_SIZE = 1 # DEBUG 16 
 VALIDATION_BATCH_SIZE = 128
-NUM_WORKERS = 128
-NUM_EDIT_COST_PROCESSES = 26
+NUM_WORKERS = 12
+NUM_EDIT_COST_PROCESSES = 14
 
 LR = 2e-5
 ADAMW_BETAS = (0.9, 0.999)
@@ -350,8 +353,9 @@ def grpo_update(old_policy: GRPOViTOMR, policy_theta: GRPOViTOMR, optimizer, bat
         for _ in range(update_epochs):
             print(f"DEBUG decoder unembed between updates: {old_policy.decoder.unembed}")
             # generate next token logits at each time step by using rollouts in a teacher forcing step
-            theta_logits = policy_theta.decoder(right_shifted_rollouts, img_latent, rollout_attention_mask, latent_attention_mask, checkpoint_grads=True)
-            print(theta_logits.requires_grad)
+            theta_logits = policy_theta.decoder.forward(right_shifted_rollouts, img_latent, rollout_attention_mask, latent_attention_mask, checkpoint_grads=True)
+            print(f"DEBUG {torch.is_grad_enabled()} {any(p.requires_grad for p in policy_theta.parameters())}")
+            print(f"DEBUG {theta_logits.requires_grad}")
             grpo_objective = calc_grpo_objective(theta_logits, rollouts, rollout_attention_mask, old_policy_log_probs, advantages, update_config.epsilon, num_groups)
             entropy_bonus = calc_entropy_bonus(theta_logits, rollout_attention_mask, vocab_size)
 
@@ -515,7 +519,7 @@ if __name__ == "__main__":
     # remake decoder into variant that supports KV caching so GRPO doesn't take forever. Caches need to support a max batch of 
     # batch size * number of rollouts per group since groups are flattened during inference
     max_batch_size = TRAIN_BATCH_SIZE * INITIAL_ROLLOUT_CONFIG.group_size
-    decoder = teacher_forced_vitomr.decoder.to_cached_version(max_batch_size)
+    decoder = teacher_forced_vitomr.decoder.to_cached_version(max_batch_size, torch.bfloat16)
 
     teacher_forced_state_dict = torch.load(TEACHER_FORCED_STATE_DICT_PATH)
 

tests/test_kv_caching.py
@@ -329,7 +329,7 @@ def test_omr_decoder_with_caching():
         "mlp_dim": 48
     }
     uncached_decoder = OMRDecoder(**omr_decoder_kwargs)
-    cached_decoder = OMRDecoder(**omr_decoder_kwargs, use_caching=True, max_batch_size=batch_size)
+    cached_decoder = OMRDecoder(**omr_decoder_kwargs, use_caching=True, max_batch_size=batch_size, cache_dtype=torch.float)
     uncached_decoder.eval()
     cached_decoder.eval()
     cached_decoder.load_state_dict(uncached_decoder.state_dict())
@@ -361,7 +361,10 @@ def test_omr_decoder_with_caching():
     assert torch.allclose(cached_out, uncached_out, atol=1e-6, rtol=1e-5)
 
     # test uncached path works fine
-    cached_out_tf = uncached_decoder.generate(full_sequence, latent, latent_attention_mask=latent_mask)
+    cached_out_tf = cached_decoder.generate(full_sequence, latent, latent_attention_mask=latent_mask)
+    assert(cached_out_tf.requires_grad)
+    cached_out_tf = cached_decoder.forward(full_sequence, latent, None, latent_attention_mask=latent_mask)
+    assert(cached_out_tf.requires_grad)
     assert torch.allclose(cached_out_tf, uncached_out, atol=1e-6, rtol=1e-5)
 
     # test trying to go past max decoder limit